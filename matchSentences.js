b2bad59d717057a083a48db5ba1f915d68ce10f7
Irving Cabello <irvollo@gmail.com>
2019-02-19 01:54:17 -0600

    MatchSentences, MatchPinyin and reorder senteces bots

---
+++ matchSentences.js
@@ -0,0 +1,114 @@
+const Nightmare = require('nightmare');
+const cheerio = require('cheerio');
+const Twit = require('twit');
+var randomWords = require('random-words');
+
+require('dotenv').config();
+
+const config = {
+    /* Be sure to update the .env file with your API keys. See how to get them: https://botwiki.org/tutorials/how-to-create-a-twitter-app */
+    twitter: {
+        consumer_key: process.env.CONSUMER_KEY,
+        consumer_secret: process.env.CONSUMER_SECRET,
+        access_token: process.env.ACCESS_TOKEN,
+        access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+    }
+};
+
+const T = new Twit(config.twitter);
+
+const word = randomWords();
+const nightmare = Nightmare({show: true});
+const query = `https://dict.naver.com/linedict/zhendict/#/cnen/example?query=${word}`;
+
+// orders
+function exerciseOrder(sentences) {
+    const nums = new Set();
+    while(nums.size !== sentences.length) {
+        nums.add(Math.floor(Math.random() * sentences.length));
+    }
+    return Array.from(nums);
+}
+
+function parseTweet(sentenceList, order) {
+    const options = ["A", "B", "C", "D"];
+    const header = "Match the sentences!"
+    let englishSentences  = ""
+    let hanziSentences = ""
+    sentenceList.map((sentence, index) => {
+        const randomSentence = sentence.englishSentence;
+        englishSentences = englishSentences + `\n${options[index]}) ${randomSentence}`;
+    })
+    sentenceList.map((sentence, index) => {
+        const randomAnswer = sentenceList[order[index]].hanziSentence;
+        hanziSentences = hanziSentences + `\n${index + 1}) ${randomAnswer}`;
+    })
+    
+    const tweet = `${header}\n${englishSentences}\n${hanziSentences}`;
+    return tweet;
+}
+
+function parseAnswer(senteceList, orderB) {
+    const options = ["A", "B", "C"];
+    let answers = "[Sentence Answer]:\n\n";
+    options.map((option, index) => {
+        answers = answers + `${option}-${orderB[index] + 1} (${senteceList[index].pinyinSentence})\n`       
+    });
+    return answers;
+} 
+
+
+// Define the array where the sentences will be posted
+const sentenceList = [];
+
+// Request making using nightmare
+nightmare
+    .goto(query)
+    .wait(3000)
+    .goto(query)
+    .wait('body')
+    .wait('.section_search')
+    .wait('.srch_result')
+    .wait(2000)
+        .evaluate(() => document.querySelector('.example_lst').innerHTML)
+    .end()
+    .then(response => {
+        const $ = cheerio.load(response);
+        $('.exam').each(function(index, element) {
+            let hanziSentence = "";
+            $(element).find('.stc')
+                .each((index, stcElement) => {
+                    const currHanzi = $(stcElement).find($('.autolink')).text().trim();
+                    hanziSentence = hanziSentence + currHanzi;
+                });
+            const pinyinSentence = $(element).find($('.pinyin')).text().trim();
+            const englishSentence = $(element).find($('.trans')).text().trim();
+            if (englishSentence.length > 50 || hanziSentence.length > 12 ) {return}
+            const sentence = {pinyinSentence, englishSentence, hanziSentence} 
+            sentenceList.push(sentence);   
+        })
+    })
+    .then(() => {
+        const sentencesUsed = sentenceList.slice(0,3);
+        const senteceOrder = exerciseOrder(sentencesUsed);
+        const tweet = parseTweet(sentencesUsed, senteceOrder);
+        const answer = parseAnswer(sentencesUsed, senteceOrder);
+        console.log(tweet);
+        console.log(answer);
+        T.post('statuses/update', { status: tweet }, function(err, data, response) {
+            if (!err) {
+                const twitId = data.id_str;
+                console.log('Trying to reply to: ', twitId);
+
+                setTimeout(() => {
+                    T.post('statuses/update', { status: answer, in_reply_to_status_id: twitId, auto_populate_reply_metadata: true }, function(err, data, response) {
+                        if (!err) {
+                                console.log('Success!');
+                        } else {
+                            console.log(err);
+                        }
+                    })
+                }, 2000)
+            }
+        })
+    })
---
+++ reorderSentence.js
@@ -0,0 +1,118 @@
+const Nightmare = require('nightmare');
+const cheerio = require('cheerio');
+const Twit = require('twit');
+var randomWords = require('random-words');
+
+require('dotenv').config();
+
+const config = {
+    /* Be sure to update the .env file with your API keys. See how to get them: https://botwiki.org/tutorials/how-to-create-a-twitter-app */
+    twitter: {
+        consumer_key: process.env.CONSUMER_KEY,
+        consumer_secret: process.env.CONSUMER_SECRET,
+        access_token: process.env.ACCESS_TOKEN,
+        access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+    }
+};
+
+const T = new Twit(config.twitter);
+
+const word = randomWords();
+const nightmare = Nightmare({show: true});
+const query = `https://dict.naver.com/linedict/zhendict/#/cnen/example?query=${word}`;
+
+// orders
+function exerciseOrder(sentences) {
+    const nums = new Set();
+    while(nums.size !== sentences.length) {
+        nums.add(Math.floor(Math.random() * sentences.length));
+    }
+    return Array.from(nums);
+}
+
+function reorderSentence(order, sentence) {
+    let reorderedSentence = [];
+    sentence.map((char, index) => {
+        const currOrder = order[index];
+        const currChar = sentence[currOrder];
+        reorderedSentence.push(currChar);
+    })
+    return reorderedSentence;
+}
+
+function parseTweet(english, sentence) {
+    const options = ["A", "B", "C","D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N"];
+    const header = "Reorder the sentence!"
+    let parsedSentence  = ""
+    sentence.map((char, index) => {
+        parsedSentence = parsedSentence + `${options[index]}) ${char} ${(index !== 0 && index % 4 === 0) ? '\n' : ''}`
+    })
+
+    const tweet = `${header}\n\n[${english}]\n\n${parsedSentence}\n`;
+    return tweet;
+}
+
+function parseAnswer(sentence, order) {
+    const options = ["A", "B", "C","D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N"];
+    let answers = "[Order Sentence Answer]:\n\n";
+    order.map((orderIndex, index) => {
+        answers = answers + `${options[orderIndex]}(${sentence.hanziSentenceOrdered[index]})${index + 1 === order.length ? "" : "-"}` 
+    });
+    return `${answers}\n${sentence.pinyinSentence}`;
+} 
+
+
+
+// Request making using nightmare
+nightmare
+    .goto(query)
+    .wait(3000)
+    .goto(query)
+    .wait('body')
+    .wait('.section_search')
+    .wait('.srch_result')
+    .wait(2000)
+        .evaluate(() => document.querySelector('.example_lst').innerHTML)
+    .end()
+    .then(response => {
+        const $ = cheerio.load(response);
+        const sentences = [];
+        $('.exam').each(function(index, element) {
+            const hanziSentenceOrdered = [];
+            $(element).find('.stc .autolink')
+                .each((index, stcElement) => {
+                    const currHanzi = $(stcElement).text().trim();
+                    hanziSentenceOrdered.push(currHanzi);
+                });
+            const englishSentence = $(element).find($('.trans')).text().trim();
+            const pinyinSentence = $(element).find($('.pinyin')).text().trim();
+            sentences.push({hanziSentenceOrdered, englishSentence, pinyinSentence});
+        })
+        return sentences;
+    })
+    .then((sentences) => {
+        const randomIndex = Math.floor(Math.random() * sentences.length)
+        const randomSentence = sentences[randomIndex];
+        const randomOrder = exerciseOrder(randomSentence.hanziSentenceOrdered);
+        const reorderedSentence = reorderSentence(randomOrder, randomSentence.hanziSentenceOrdered);
+        const tweet = parseTweet(randomSentence.englishSentence, reorderedSentence);
+        const answer = parseAnswer(randomSentence, randomOrder);
+        console.log(tweet);
+        console.log(answer);
+        T.post('statuses/update', { status: tweet }, function(err, data, response) {
+            if (!err) {
+                const twitId = data.id_str;
+                console.log('Trying to reply to: ', twitId);
+
+                setTimeout(() => {
+                    T.post('statuses/update', { status: answer, in_reply_to_status_id: twitId, auto_populate_reply_metadata: true }, function(err, data, response) {
+                        if (!err) {
+                                console.log('Success!');
+                        } else {
+                            console.log(err);
+                        }
+                    })
+                }, 2000)
+            }
+        })
+    })
